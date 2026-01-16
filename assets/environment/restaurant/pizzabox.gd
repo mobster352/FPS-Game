1d7335063134cedfcfd954cd68713f67a7959ec9
Hubert Walton <hubertwalton352@gmail.com>
2026-01-16 17:54:30 -0500

    can now box pizzas

--- assets/environment/restaurant/pizzabox.gd
+++ assets/environment/restaurant/pizzabox.gd
@@ -3,11 +3,23 @@ class_name PizzaBox
 
 @export var lid: MeshInstance3D
 @export var speed := 1.5
+@export var pizza_slot: Node3D
 
 var is_open := false
 var is_cook := false
 var is_interact := false
 var elapsed := 0.0
+
+func _ready() -> void:
+	super._ready()
+	if mesh.has_meta("pizza"):
+		var pizza_type:GlobalVar.PIZZA_TYPE = mesh.get_meta("pizza")
+		if pizza_type == GlobalVar.PIZZA_TYPE.PEPPERONI:
+			pizza_slot.add_child(preload("res://assets/kaykit/restaurant/food_pizza_pepperoni_plated.gltf").instantiate())
+		if pizza_type == GlobalVar.PIZZA_TYPE.MUSHROOM:
+			pizza_slot.add_child(preload("res://assets/kaykit/restaurant/food_pizza_mushroom_plated.gltf").instantiate())
+		if pizza_type == GlobalVar.PIZZA_TYPE.CHEESE:
+			pizza_slot.add_child(preload("res://assets/kaykit/restaurant/food_pizza_cheese_plated.gltf").instantiate())
 
 func _process(delta: float) -> void:
 	if elapsed >= 1.0:
@@ -22,9 +34,40 @@ func _process(delta: float) -> void:
 		elapsed += speed * delta
 
 func interact() -> void:
-	if not is_interact:
-		is_interact = true
+	if disabled:
+		return
+	if player.item_slot.get_child_count() > 0:
+		player.drop_item()
+	if get_parent():
+		get_parent().remove_child(self)
+	pickup(Vector3.ZERO, Vector3(deg_to_rad(10),deg_to_rad(130),deg_to_rad(0)))
+	if pizza_slot.has_meta("pizza"):
+		player.item_slot.get_child(0).set_meta("pizza", pizza_slot.get_meta("pizza"))
+	queue_free()
 
 func cook() -> void:
 	if not is_cook:
 		is_cook = true
+
+
+func _on_pizza_detection_area_body_entered(body: Node3D) -> void:
+	if body.has_meta("name") and body.get_meta("name") == "whole_pizza" and is_open:
+		var item = body.get_parent() as Item
+		if item:
+			if pizza_slot.get_child_count() == 0:
+				var _mesh = item.get_node("body/mesh")
+				_mesh.position = Vector3.ZERO
+				_mesh.rotation = Vector3.ZERO
+				if _mesh.get_parent():
+					_mesh.get_parent().remove_child(_mesh)
+				if item.mesh.has_meta("name"):
+					if item.mesh.get_meta("name") == "food_ingredient_pepperoni_pizza_mesh":
+						pizza_slot.set_meta("pizza", GlobalVar.PIZZA_TYPE.PEPPERONI)
+					elif item.mesh.get_meta("name") == "food_ingredient_mushroom_pizza_mesh":
+						pizza_slot.set_meta("pizza", GlobalVar.PIZZA_TYPE.MUSHROOM)
+					elif item.mesh.get_meta("name") == "food_ingredient_cheese_pizza_mesh":
+						pizza_slot.set_meta("pizza", GlobalVar.PIZZA_TYPE.CHEESE)
+					else:
+						pizza_slot.set_meta("pizza", GlobalVar.PIZZA_TYPE.NONE)
+				pizza_slot.add_child(_mesh)
+			item.shrink_and_free(0, 0.25)
