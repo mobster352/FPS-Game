c0ab9b7f890f5b90a13a8b4e9102ec3bb0efbb34
Hubert Walton <hubertwalton352@gmail.com>
2026-04-22 17:48:04 -0400

    improved npc pathing

--- entities/npc/thief_spawner.gd
+++ entities/npc/thief_spawner.gd
@@ -21,5 +21,5 @@ func _on_thief_timer_timeout() -> void:
 	thief.end_path_marker = endPathMarker
 	thief.position = position + Vector3(0, 0, randf_range(-0.5,0.5))
 	thief.rotation = rotation
-	%ThiefTimer.wait_time = randf_range(3, 15)
+	%ThiefTimer.wait_time = randf_range(15, 30)
 	get_parent().add_child(thief)
--- environment/npc_spawner.gd
+++ environment/npc_spawner.gd
@@ -16,6 +16,7 @@ var skins:Array[String] = [
 func _ready() -> void:
 	GlobalSignal.open_store.connect(_open_store)
 	GlobalSignal.close_store.connect(_close_store)
+	npcSpawnTimer.wait_time = randi_range(3, 10)
 	
 func _open_store() -> void:
 	npcSpawnTimer.start()
@@ -24,11 +25,15 @@ func _close_store() -> void:
 	npcSpawnTimer.stop()
 
 func _on_npc_spawn_timer_timeout() -> void:
+	var stagger_chance = randi_range(0,3)
+	if stagger_chance == 0:
+		npcSpawnTimer.wait_time = 3
+		return
 	var npc_dummy:NPC_Dummy = preload("uid://dxnl4jurpfddl").instantiate()
 	npc_dummy.dummy_scene = load(skins.pick_random())
 	npc_dummy.endPathMarker = endPathMarker
 	npc_dummy.level_ui = level_ui
 	npc_dummy.position = position + Vector3(0, 0, randf_range(-0.5,0.5))
 	npc_dummy.rotation = rotation
-	npcSpawnTimer.wait_time = randf_range(3, 15)
+	npcSpawnTimer.wait_time = randi_range(5, 15)
 	get_parent().add_child(npc_dummy)
