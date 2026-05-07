abfda60f287e78fa6574ea95df1c660af0197683
Hubert Walton <hubertwalton352@gmail.com>
2026-05-07 19:28:29 -0400

    added lights to scene; adjust table menu

--- assets/environment/restaurant/menu.gd
+++ assets/environment/restaurant/menu.gd
@@ -1,8 +1,8 @@
 extends Node3D
 class_name Menu
 
-@export var table_label: Label
-@export var order_label: Label
+#@export var table_label: Label
+#@export var order_label: Label
 @export var table: Table
 
 var table_id: int
@@ -18,13 +18,13 @@ func _add_order(_table_id:int, _food_id:int) -> void:
 	if table_id == _table_id:
 		for food_item in GlobalVar.food_items:
 			if food_item.food_id == _food_id:
-				order_label.text = food_item.food_name
+				#order_label.text = food_item.food_name
 				food_id = _food_id
 
 
 func _remove_order_from_list(_table_id:int) -> void:
 	if table_id == _table_id:
-		order_label.text = ""
+		#order_label.text = ""
 		var old_food_id = food_id
 		food_id = -1
 		GlobalSignal.check_restaurant_food.emit(old_food_id)
@@ -33,4 +33,5 @@ func _remove_order_from_list(_table_id:int) -> void:
 func _send_table_id(_table: Table, _table_id: int) -> void:
 	if table == _table:
 		table_id = _table_id
-		table_label.text = "Table " + str(table_id)
+		#table_label.text = "Table " + str(table_id)
+		%TableNumber.text = str(table_id)
